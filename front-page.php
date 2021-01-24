cb0d2c7b8ba9607ed43546808660f2f8851a9b7a
Erik <erik.jung@5pectre.com>
2021-01-24 03:43:11 +0100

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section id="title" class="light-background">
+    <div class="w-wrapper padding-two-top padding-two-bottom">
+        <div class="centered-text">
+            <h1>Sample Section</h1>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
